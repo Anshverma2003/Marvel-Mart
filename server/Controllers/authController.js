32daa8310f483c80e548cbec204b078fd5919762
Ansh Verma <vermaansh708@gmail.com>
2024-03-18 16:51:56 +0530

    jwt auth

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -1,5 +1,12 @@
 import UserModel from "../Models/user.js";
+import validator from "validator";
 import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
+
+const generateToken = (payload) => {
+    return Jwt.sign(payload, process.env.Secret);
+}
+
 
 export const signup = async (req, res) => {
     const { firstname, lastname, email, password } = req.body;
@@ -15,23 +22,69 @@ export const signup = async (req, res) => {
     );
 
     try {
-        const savedUser = await newUser.saveUser();
-        if (savedUser.success) {
-            res.status(201).json({ message: "User created successfully" });
-        } else {
-            throw new Error("User not saved");
+
+        if (!email || !password || !firstname || !lastname) {
+            throw new Error("All field required");
         }
+        if (!validator.isEmail(email)) {
+            throw new Error("Enter a valid email");
+        }
+        if (!validator.isStrongPassword(password)) {
+            throw new Error("Enter are strong password");
+        }
+
+        let existingUser = await UserModel.checkExistingUser(email);
+
+        if (existingUser) {
+            throw new Error("Email already exists. Try logging in");
+        }
+
+        else {
+            const savedUser = await newUser.saveUser();
+            if (savedUser.success) {
+
+                const token = generateToken(newUser.firstname);
+
+                res.status(201).json({ message: "User created successfully", token });
+            } else {
+                throw new Error("User not saved");
+            }
+        }
+
     } catch (error) {
 
-        if (error.message === "Email already exists. Try logging in.") {
-            res.status(400).json({ error: error.message });
-        } else {
-            res.status(500).json({ error: "Failed to create user" });
-        }
+        res.status(500).json({ error: error.message });
     }
 };
 
 export const login = async (req, res) => {
+    const { email, password } = req.body;
 
-    res.send("login");
-}
+    try {
+        if (!email || !password) {
+            throw new Error("All fields required");
+        }
+        if (!validator.isEmail(email)) {
+            throw new Error("Enter a valid email");
+        }
+
+        const user = await UserModel.findUser(email);
+
+        if (!user) {
+            throw error;
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            throw new Error("Wrong Password");
+        }
+
+        const token = generateToken(user.email);
+
+        res.status(200).json({ message: "Login successful", token });
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
--- server/Models/user.js
+++ server/Models/user.js
@@ -8,32 +8,55 @@ class UserModel {
         this.password = password;
     }
 
+    static async checkExistingUser(email) {
+        try {
+
+            const existingUser = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+            if (existingUser.rows.length > 0) {
+                return true;
+            }
+            return false;
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
     async saveUser() {
 
         try {
-            const existingUser = await db.query("SELECT * FROM users WHERE email = $1", [this.email]);
 
-            if (existingUser.rows.length > 0) {
-                throw new Error("Email already exists. Try logging in.");
+            const result = await db.query("INSERT INTO users(id , firstname , lastname , email , password) VALUES (DEFAULT , $1 , $2 , $3, $4)",
+                [this.firstname, this.lastname, this.email, this.password]
+            );
+
+            if (!result) {
+                throw new Error("User creation failed: Database query did not return a result");
             }
-            else {
-
-                const result = await db.query("INSERT INTO users(id , firstname , lastname , email , password) VALUES (DEFAULT , $1 , $2 , $3, $4)",
-                    [this.firstname, this.lastname, this.email, this.password]
-                );
-
-                if (!result) {
-                    throw new Error("User creation failed: Database query did not return a result");
-                }
-
-                return { success: true };
-            }
+            return { success: true };
 
         } catch (err) {
 
             throw err;
         }
 
+    }
+
+    static async findUser(email){
+        try{
+            const checkUser = await db.query("SELECT * FROM users WHERE email = $1" , [email]);
+            if(checkUser.rows.length > 0){
+
+                return checkUser.rows[0];
+            }
+            else{
+                throw new Error("User not Found. Try signing in");
+            }
+        }
+        catch(error){
+            throw error;
+        }
     }
 }
 
