283586ff4a3c4b9525346026bc05282faeaefc14
Ansh Verma <vermaansh708@gmail.com>
2024-04-15 16:03:35 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -9,8 +9,22 @@ import cors from 'cors';
 const port = 8080;
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://marvel-mart-ten.vercel.app',
+  // Add other origins as needed
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(authRouter);
@@ -18,7 +32,6 @@ app.use(productRouter);
 app.use(productIDRouter);
 app.use(cartRouter);
 app.use(addressRouter);
-
 
 app.listen(port , ()=>{
     console.log(`Server is up and running on ${port}`);
